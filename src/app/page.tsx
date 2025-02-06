1bed798f755376d3763ae794b647132aff32bed5
Umarhyall <umarbaharun941@gmail.com>
2025-02-06 21:54:57 +0700

    feat: add breadcrumbs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Breadcrumb from '@/components/atoms/Breadcrumbs'
 import InitialComponent from '@/components/organisms/InitialComponent'
 
-export default function ProductsPage() {
-  return (
-    <div className="p-6">
-      <Breadcrumb items={[{ label: 'Home', href: '/' }, { label: 'Components' }]} />
-      <InitialComponent />
-    </div>
-  )
+export default function Home() {
+  return <InitialComponent />
 }
