c26adff829fb7f81f411605501b8dabe2c0b3d25
Umarhyall <umarbaharun941@gmail.com>
2025-02-06 14:22:50 +0700

    feat: add breadcrumbs component using shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Breadcrumb from '@/components/atoms/Breadcrumbs'
 import InitialComponent from '@/components/organisms/InitialComponent'
 
-export default function Home() {
-  return <InitialComponent />
+export default function ProductsPage() {
+  return (
+    <div className="p-6">
+      <Breadcrumb items={[{ label: 'Home', href: '/' }, { label: 'Components' }]} />
+      <InitialComponent />
+    </div>
+  )
 }
