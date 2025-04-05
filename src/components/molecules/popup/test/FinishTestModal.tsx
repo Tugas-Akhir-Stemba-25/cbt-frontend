0e537c6a61ccc645102eabcf37f297d45cb71cf7
Evandra Raditya <evandraraditya049@gmail.com>
2025-04-05 13:17:39 +0700

    feat(exam): implement finish test functionality with success and error handling

--- src/components/molecules/popup/test/FinishTestModal.tsx
+++ src/components/molecules/popup/test/FinishTestModal.tsx
@@ -1,4 +1,11 @@
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+import { toast } from 'sonner'
+
+import { useFinishWork } from '@/http/work/finish-work'
+
+import useWorkHashStore from '@/stores/useWorkHashStore'
 
 import {
   AlertDialog,
@@ -16,6 +23,28 @@ interface FinishTestModalProps {
 }
 
 const FinishTestModal = ({ isOpen, onOpenChange, isTimeout = false }: FinishTestModalProps) => {
+  const router = useRouter()
+
+  const { hash } = useWorkHashStore()
+
+  const { mutate: finishTest, isPending } = useFinishWork({
+    onSuccess: (res) => {
+      toast.success('Sukses', {
+        description: res.meta.message
+      })
+      router.push(`/dashboard/student/work/${hash}/result`)
+    },
+    onError: (err) => {
+      toast.error('Gagal', {
+        description: err.response?.data?.meta?.message || err.response?.data.meta.error
+      })
+    }
+  })
+
+  const handleFinishTest = () => {
+    finishTest({ hash: hash as string })
+  }
+
   return (
     <AlertDialog open={isOpen} onOpenChange={onOpenChange}>
       <AlertDialogContent>
@@ -53,7 +82,9 @@ const FinishTestModal = ({ isOpen, onOpenChange, isTimeout = false }: FinishTest
           </div>
         </AlertDialogHeader>
         <AlertDialogFooter className="w-full">
-          <Button className="w-full">Submit</Button>
+          <Button onClick={handleFinishTest} isLoading={isPending} className="w-full">
+            Submit
+          </Button>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
