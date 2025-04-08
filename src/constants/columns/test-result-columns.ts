228740b570a49da7d0bf6c7ad5500f51da556a9f
Umarhyall <umarbaharun941@gmail.com>
2025-04-08 10:57:15 +0700

    feat: restart student exam

--- src/constants/columns/test-result-columns.ts
+++ src/constants/columns/test-result-columns.ts
@@ -1,5 +1,6 @@
 import { ColumnDef } from '@tanstack/react-table'
 
+import TestRestartColumn from '@/components/atoms/button/RestartTestButton'
 import TestDurationColumn from '@/components/atoms/column/test/TestDurationColumn'
 import TestHistoryStatusColumn from '@/components/atoms/column/test/TestHistoryStatusColumn'
 
@@ -21,29 +22,32 @@ export const testResultColumns: ColumnDef<TestResultFieldRowProps>[] = [
   {
     header: 'Soal Benar',
     enableSorting: false,
-    accessorFn: (row) => (row.history && row.history.correct_answer ? row.history.correct_answer : '-')
+    accessorFn: (row) => row.history?.correct_answer ?? '-'
   },
   {
     header: 'Soal Salah',
     enableSorting: false,
     accessorFn: (row) =>
-      row.history && row.history.total && row.history.correct_answer
-        ? row.history.total - row.history.correct_answer
-        : '-'
+      row.history?.total && row.history?.correct_answer ? row.history.total - row.history.correct_answer : '-'
   },
   {
     header: 'Total Soal',
     enableSorting: false,
-    accessorFn: (row) => (row.history && row.history.total ? row.history.total : '-')
+    accessorFn: (row) => row.history?.total ?? '-'
   },
   {
     header: 'Nilai',
     enableSorting: false,
-    accessorFn: (row) => (row.history && row.history.grade ? row.history.grade : '-')
+    accessorFn: (row) => row.history?.grade ?? '-'
   },
   {
     header: 'Status',
     enableSorting: false,
     cell: TestHistoryStatusColumn
+  },
+  {
+    header: 'Aksi',
+    enableSorting: false,
+    cell: TestRestartColumn
   }
 ]
---
+++ src/http/test/get-test-restart.ts
@@ -0,0 +1,35 @@
+import { useMutation, UseMutationOptions } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+
+import { api } from '@/lib/axios'
+
+import { Metadata } from '@/types/common/metadata'
+import { TestResult } from '@/types/test/test'
+
+export interface RestartTestPayload {
+  testId: number
+  userId: number
+}
+
+export interface RestartTestResponse {
+  meta: Metadata
+  data: TestResult
+}
+
+export const restartTestHandler = async ({ testId, userId }: RestartTestPayload): Promise<RestartTestResponse> => {
+  console.log('DEBUG >> restartTestHandler:', { testId, userId })
+  const response = await api.delete<RestartTestResponse>(`/tests/${testId}/restart`, {
+    data: { user_id: userId }
+  })
+
+  return response.data
+}
+
+export const useRestartTest = (
+  options?: Partial<UseMutationOptions<RestartTestResponse, AxiosError<any>, RestartTestPayload>>
+) => {
+  return useMutation({
+    mutationFn: restartTestHandler,
+    ...options
+  })
+}
