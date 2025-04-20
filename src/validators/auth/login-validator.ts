8bc67ed62be417de4957f65ae4d57209c52e703c
Evandra Raditya <evandraraditya049@gmail.com>
2025-04-21 06:31:10 +0700

    fix(validator): update password validation to remove min char

--- src/validators/auth/login-validator.ts
+++ src/validators/auth/login-validator.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
   username: z.string().min(1, 'Username tidak boleh kosong'),
-  password: z.string().nonempty('Password tidak boleh kosong').min(8, 'Password minimal 8 karakter')
+  password: z.string().nonempty('Password tidak boleh kosong')
 })
 
 export type LoginType = z.infer<typeof loginSchema>
